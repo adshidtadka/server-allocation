c1960ab1a49006e5bca17d11284f9145c99f190c
adshidtadka <nextage900fx@gmail.com>
2019-07-02 14:09:32 +0900

    create parameter class

--- src/server_allocation.py
+++ src/server_allocation.py
@@ -18,8 +18,7 @@ class Constant:
     CAPACITY_MAX = 400
 
 
-class Problem:
-
+class Parameter:
     def __init__(self, seed):
         np.random.seed(seed)
         self.create_input()
@@ -47,29 +46,34 @@ class Problem:
         df_v_s['capacity'] = m_s
         self.df_v_s = df_v_s
 
-    def set_input(self):
+
+class Problem:
+
+    def set_input(param):
         # optimization problem
-        self.problem = LpProblem()
+        problem = LpProblem()
 
         # decision variables
-        self.df_e_u['variable'] = [LpVariable('x_us%d' % i, cat=LpBinary) for i in self.df_e_u.index]
-        self.df_e_s['variable'] = [LpVariable('x_st%d' % i, cat=LpBinary) for i in self.df_e_s.index]
-        self.df_v_s['variable'] = [LpVariable('y%d' % i, cat=LpBinary) for i in self.df_v_s.index]
-        self.D_u = LpVariable('D_u', cat=LpInteger)
-        self.D_s = LpVariable('D_s', cat=LpInteger)
+        param.df_e_u['variable'] = [LpVariable('x_us%d' % i, cat=LpBinary) for i in param.df_e_u.index]
+        param.df_e_s['variable'] = [LpVariable('x_st%d' % i, cat=LpBinary) for i in param.df_e_s.index]
+        param.df_v_s['variable'] = [LpVariable('y%d' % i, cat=LpBinary) for i in param.df_v_s.index]
+        param.D_u = LpVariable('D_u', cat=LpInteger)
+        param.D_s = LpVariable('D_s', cat=LpInteger)
 
         # objective function
-        self.problem += 2 * self.D_u + self.D_s
+        problem += 2 * param.D_u + param.D_s
 
         # constraints
-        self.problem = self.create_constraints(self.problem)
+        problem = Problem.create_constraints(param, problem)
 
-    def solve_by_ilp(self):
+        return problem
+
+    def solve_by_ilp(problem):
         # solve
         try:
             print('-------- t_0 --------')
             t_0 = time.process_time()
-            self.problem.solve(CPLEX_CMD(msg=1))
+            problem.solve(CPLEX_CMD(msg=1))
             t_1 = time.process_time()
             print('\n-------- t_1 --------')
 
@@ -78,48 +82,46 @@ class Problem:
             print(CPLEX_CMD().path, 'is not installed')
 
         # result
-        if self.problem.status == 1:
-            print('objective function is = ', value(self.problem.objective))
-            self.df_e_u.variable = self.df_e_u.variable.apply(value)
-            # print(self.df_e_u[self.df_e_u.variable >= 1])
+        if problem.status == 1:
+            print('objective function is = ', value(problem.objective))
         else:
-            print('status code is = ', self.problem.status)
+            print('status code is = ', problem.status)
 
-    def create_constraints(self, m):
+    def create_constraints(param, m):
         # constraints
         # (1b)
-        for k, v in self.df_e_u.groupby('user'):
+        for k, v in param.df_e_u.groupby('user'):
             m += lpSum(v.variable) == 1
 
         # (1c)
-        for k, v in self.df_e_u.groupby('server'):
-            m += lpSum(v.variable) <= self.df_v_s['capacity'][k]
+        for k, v in param.df_e_u.groupby('server'):
+            m += lpSum(v.variable) <= param.df_v_s['capacity'][k]
 
         # (1d)
-        for k, v in self.df_e_u.iterrows():
-            m += v.variable * v.delay <= self.D_u
+        for k, v in param.df_e_u.iterrows():
+            m += v.variable * v.delay <= param.D_u
 
         # (1e)
-        for k, v in self.df_e_s.iterrows():
-            m += v.variable * v.delay <= self.D_s
+        for k, v in param.df_e_s.iterrows():
+            m += v.variable * v.delay <= param.D_s
 
         # (1f)
-        for k, v in self.df_e_u.groupby('user'):
-            for l, w in self.df_v_s.iterrows():
+        for k, v in param.df_e_u.groupby('user'):
+            for l, w in param.df_v_s.iterrows():
                 m += w.variable >= v.variable
 
         # (1g)
-        for k, v in self.df_e_s.iterrows():
-            m += self.df_v_s.iloc[v.server_1].variable + \
-                self.df_v_s.iloc[v.server_2].variable - 1 <= v.variable
+        for k, v in param.df_e_s.iterrows():
+            m += param.df_v_s.iloc[v.server_1].variable + \
+                param.df_v_s.iloc[v.server_2].variable - 1 <= v.variable
 
         # (1h)
-        for k, v in self.df_e_s.iterrows():
-            m += v.variable <= self.df_v_s.iloc[v.server_1].variable
+        for k, v in param.df_e_s.iterrows():
+            m += v.variable <= param.df_v_s.iloc[v.server_1].variable
 
         # (1i)
-        for k, v in self.df_e_s.iterrows():
-            m += v.variable <= self.df_v_s.iloc[v.server_2].variable
+        for k, v in param.df_e_s.iterrows():
+            m += v.variable <= param.df_v_s.iloc[v.server_2].variable
 
         return m
 
@@ -127,13 +129,13 @@ class Problem:
 def main():
 
     # create input
-    problem = Problem(1)
+    param = Parameter(1)
 
     # set input to problem
-    problem.set_input()
+    problem = Problem.set_input(param)
 
     # solve by ilp
-    problem.solve_by_ilp()
+    Problem.solve_by_ilp(problem)
 
 
 if __name__ == '__main__':
