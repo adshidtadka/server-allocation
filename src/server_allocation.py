faa363d83120f07a0762d0d98ba791bbce95e32a
adshidtadka <nextage900fx@gmail.com>
2019-07-02 13:37:41 +0900

    create constant class

--- src/server_allocation.py
+++ src/server_allocation.py
@@ -8,7 +8,7 @@ from itertools import product
 from pulp import *
 
 
-class Problem:
+class Constant:
 
     # constants
     USER_NUM = 800
@@ -17,15 +17,18 @@ class Problem:
     DELAY_SERVER = 1
     CAPACITY_MAX = 400
 
+
+class Problem:
+
     def __init__(self, seed):
         np.random.seed(seed)
 
         # inputs
-        e_u = list(product(range(self.USER_NUM), range(self.SERVER_NUM)))
-        e_s = list(itertools.combinations(list(range(0, self.SERVER_NUM)), 2))
-        d_us = np.random.randint(0, self.DELAY_MAX, (self.USER_NUM, self.SERVER_NUM))
-        v_s = list(range(0, self.SERVER_NUM))
-        m_s = np.random.randint(0, self.CAPACITY_MAX, self.SERVER_NUM)
+        e_u = list(product(range(Constant.USER_NUM), range(Constant.SERVER_NUM)))
+        e_s = list(itertools.combinations(list(range(0, Constant.SERVER_NUM)), 2))
+        d_us = np.random.randint(0, Constant.DELAY_MAX, (Constant.USER_NUM, Constant.SERVER_NUM))
+        v_s = list(range(0, Constant.SERVER_NUM))
+        m_s = np.random.randint(0, Constant.CAPACITY_MAX, Constant.SERVER_NUM)
 
         # dataframe for E_U
         df_e_u = pd.DataFrame([(i, j) for i, j in e_u], columns=['user', 'server'])
@@ -34,7 +37,7 @@ class Problem:
 
         # dataframe for E_S
         df_e_s = pd.DataFrame([(i, j) for i, j in e_s], columns=['server_1', 'server_2'])
-        df_e_s['delay'] = self.DELAY_SERVER
+        df_e_s['delay'] = Constant.DELAY_SERVER
         self.df_e_s = df_e_s
 
         # dataframe for V_S
