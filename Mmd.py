73013356be318f7cf344b80ad7ddd3620e2ef335
adshidtadka <nextage900fx@gmail.com>
2019-07-03 13:51:44 +0900

    avoid to use dataframe

--- Mmd.py
+++ Mmd.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 import time
 
 import Constant
@@ -7,26 +6,13 @@ from Parameter import Parameter
 
 
 class Mmd:
-
-    def __init__(self, param):
-        self.set_input(param)
-
-    def set_input(self, param):
-        param.df_e_u['x'] = 0
-        param.df_e_s['x'] = 0
-        param.D_u = 0
-
-        # for step 1 an step 2
-        self.param_1 = param
-        self.param_2 = param
-
-    def start_algorithm(self):
+    def start_algorithm(self, param):
         print('-------- t_0 --------')
         t_0 = time.process_time()
 
-        L_1 = self.one_server_case(self.param_1)
+        L_1 = self.one_server_case(param)
         print(L_1)
-        L_2 = self.multiple_server_case(self.param_2)
+        # L_2 = self.multiple_server_case(self.param_2)
 
         t_1 = time.process_time()
         print('\n-------- t_1 --------')
@@ -38,9 +24,9 @@ class Mmd:
 
         # allocate all user and get L_1
         dic_l = dict()
-        for k, v in param.df_v_s.iterrows():
-            if v['capacity'] >= param.USER_NUM:
-                D_u = param.df_e_u[param.df_e_u.server == k]['delay'].max()
+        for k, v in enumerate(param.m_s):
+            if v >= param.USER_NUM:
+                D_u = param.d_us[:, k].max()
                 dic_l[k] = D_u
 
         # search minimum D_u
@@ -48,10 +34,6 @@ class Mmd:
             s = min(dic_l, key=dic_l.get)
         else:
             return None
-
-        # set x and D_u
-        param.df_e_u.loc[param.df_e_u.server == s, 'x'] = 1
-        param.D_u = dic_l[s]
 
         return dic_l[s]
 
@@ -73,10 +55,10 @@ class Mmd:
 
 def main():
     # set input to algorithm
-    mmd = Mmd(Parameter(Constant.SEED))
+    mmd = Mmd()
 
     # start algorithm
-    mmd.start_algorithm()
+    mmd.start_algorithm(Parameter(Constant.SEED))
 
 
 if __name__ == '__main__':
