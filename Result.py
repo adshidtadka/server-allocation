215293cde3d7ed9a0c10580373539d623405932d
gky360 <gky360@gmail.com>
2019-07-06 19:46:29 +0900

    get result を関数化け

--- Result.py
+++ Result.py
@@ -31,6 +31,25 @@ class Result:
             print(str(sim_range) + ' set.')
             return sim_range
 
+    def get_result(self):
+        result = []
+        for i in self.sim_range:
+            # create param
+            param = Parameter(Constant.SEED)
+            param.set_user_num(i)
+            param.create_input()
+
+            # solve by algorithm
+            mmd = Mmd(param)
+            cpu_time_mmd = mmd.start_algorithm(param)
+
+            result.append([param.USER_NUM, param.SERVER_NUM, param.CAPACITY, cpu_time_mmd])
+            print(result)
+        f = open('result/' + self.sim_name + '.csv', 'w')
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerows(result)
+        f.close()
+
 
 if not os.path.exists('result'):
     os.mkdir('result')
@@ -40,56 +59,10 @@ sim_server = Result('server')
 sim_capacity = Result('capacity')
 
 if sim_user.is_execute:
-    result_user = []
-    for i in range(100, 500, 50):
-        # create param
-        param = Parameter(Constant.SEED)
-        param.set_user_num(i)
-        param.create_input()
-
-        # solve by algorithm
-        mmd = Mmd(param)
-        cpu_time_mmd = mmd.start_algorithm(param)
-
-        result_user.append([param.USER_NUM, param.SERVER_NUM, param.CAPACITY, cpu_time_mmd])
-        print(result_user)
-    f = open('result/user.csv', 'w')
-    writer = csv.writer(f, lineterminator='\n')
-    writer.writerows(result_user)
-    f.close()
+    sim_user.get_result()
 
 if sim_server.is_execute:
-    result_server = []
-    for i in range(10, 20, 1):
-        param = Parameter(Constant.SEED)
-        param.set_server_num(i)
-        param.create_input()
-
-        # solve by algorithm
-        mmd = Mmd(param)
-        cpu_time_mmd = mmd.start_algorithm(param)
-
-        result_user.append([param.USER_NUM, param.SERVER_NUM, param.CAPACITY, cpu_time_mmd])
-        print(result_server)
-    f = open('result/server.csv', 'w')
-    writer = csv.writer(f, lineterminator='\n')
-    writer.writerows(result_server)
-    f.close()
+    sim_server.get_result()
 
 if sim_capacity.is_execute:
-    result_capacity = []
-    for i in range(50, 100, 10):
-        param = Parameter(Constant.SEED)
-        param.set_capacity(i)
-        param.create_input()
-
-        # solve by algorithm
-        mmd = Mmd(param)
-        cpu_time_mmd = mmd.start_algorithm(param)
-
-        result_user.append([param.USER_NUM, param.SERVER_NUM, param.CAPACITY, cpu_time_mmd])
-        print(result_capacity)
-    f = open('result/capacity.csv', 'w')
-    writer = csv.writer(f, lineterminator='\n')
-    writer.writerows(result_capacity)
-    f.close()
+    sim_capacity.get_result()
