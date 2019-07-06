06f5e9adc8849516a36d108759f81ada5c63ad51
gky360 <gky360@gmail.com>
2019-07-06 18:11:11 +0900

    mmdの結果のみとる

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import os
 
 import Constant
 from Parameter import Parameter
-from HopcroftKarp import HopcroftKarp
-from Ilp import Ilp
 from Mmd import Mmd
 
 if not os.path.exists('result'):
@@ -18,19 +16,11 @@ for i in range(100, 500, 50):
     param.set_user_num(i)
     param.create_input()
 
-    # solve by ilp
-    ilp = Ilp(param)
-    cpu_time_ilp = ilp.solve_by_ilp()
-
-    # solve by ilp cplex
-    ilp_cplex = Ilp(param)
-    cpu_time_ilp_cplex = ilp_cplex.solve_by_ilp('cplex')
-
     # solve by algorithm
     mmd = Mmd(param)
     cpu_time_mmd = mmd.start_algorithm(param)
 
-    result_user.append([cpu_time_ilp, cpu_time_mmd, cpu_time_ilp_cplex])
+    result_user.append([param.USER_NUM, param.SERVER_NUM, param.CAPACITY, cpu_time_mmd])
     print(result_user)
 f = open('result/user.csv', 'w')
 writer = csv.writer(f, lineterminator='\n')
@@ -43,19 +33,11 @@ for i in range(10, 20, 1):
     param.set_server_num(i)
     param.create_input()
 
-    # solve by ilp
-    ilp = Ilp(param)
-    cpu_time_ilp = ilp.solve_by_ilp()
-
-    # solve by ilp cplex
-    ilp_cplex = Ilp(param)
-    cpu_time_ilp_cplex = ilp_cplex.solve_by_ilp('cplex')
-
     # solve by algorithm
     mmd = Mmd(param)
     cpu_time_mmd = mmd.start_algorithm(param)
 
-    result_server.append([cpu_time_ilp, cpu_time_mmd, cpu_time_ilp_cplex])
+    result_user.append([param.USER_NUM, param.SERVER_NUM, param.CAPACITY, cpu_time_mmd])
     print(result_server)
 f = open('result/server.csv', 'w')
 writer = csv.writer(f, lineterminator='\n')
@@ -68,19 +50,11 @@ for i in range(50, 100, 10):
     param.set_capacity(i)
     param.create_input()
 
-    # solve by ilp
-    ilp = Ilp(param)
-    cpu_time_ilp = ilp.solve_by_ilp()
-
-    # solve by ilp cplex
-    ilp_cplex = Ilp(param)
-    cpu_time_ilp_cplex = ilp_cplex.solve_by_ilp('cplex')
-
     # solve by algorithm
     mmd = Mmd(param)
     cpu_time_mmd = mmd.start_algorithm(param)
 
-    result_capacity.append([cpu_time_ilp, cpu_time_mmd, cpu_time_ilp_cplex])
+    result_user.append([param.USER_NUM, param.SERVER_NUM, param.CAPACITY, cpu_time_mmd])
     print(result_capacity)
 f = open('result/capacity.csv', 'w')
 writer = csv.writer(f, lineterminator='\n')
