43d9ba5e1d473e45ab1711c922675cc5e268f2be
adshidtadka <nextage900fx@gmail.com>
2019-10-07 17:11:19 +0900

    change renage

--- src/Constant.py
+++ src/Constant.py
@@ -4,11 +4,11 @@ import sys
 class Constant:
     SEED = 1
     INF = 999999999
-    ITERATION_NUM = 5
+    ITERATION_NUM = 50
 
     def get_range(self, variable):
         if variable == 'user':
-            return range(10, 100, 10)
+            return range(10, 50, 5)
         elif variable == 'server':
             return range(10, 20, 1)
         elif variable == 'capacity':
