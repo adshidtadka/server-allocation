b6b7d556020c79a184cec243a9b96a2ebb169f37
adshidtadka <nextage900fx@gmail.com>
2019-10-08 12:55:32 +0900

    ソルバー比較用にプロット

--- src/Plot.py
+++ src/Plot.py
@@ -32,72 +32,79 @@ class Graph:
 
 # %%
 ct = 'Computation time'
-ctm = 'Computation time with MMD'
+ctes = 'Computation time with ESUM'
 ctc = 'Computation time with CPLEX'
 ctg = 'Computation time with GLPK'
+ctsc = 'Computation time with SCIP'
 un = 'User number'
 sn = 'Server number'
 cp = 'Capacity'
 
 # %%
-df_user_s_10 = pd.read_csv("result/user{'server': 10, 'capacity': 50}.csv", names=(un, ct))
-df_user_s_15 = pd.read_csv("result/user{'server': 15, 'capacity': 50}.csv", names=(un, ct))
-df_user_s_20 = pd.read_csv("result/user{'server': 20, 'capacity': 50}.csv", names=(un, ct))
+# df_user_s_10 = pd.read_csv("result/user{'server': 10, 'capacity': 50}.csv", names=(un, ct))
+# df_user_s_15 = pd.read_csv("result/user{'server': 15, 'capacity': 50}.csv", names=(un, ct))
+# df_user_s_20 = pd.read_csv("result/user{'server': 20, 'capacity': 50}.csv", names=(un, ct))
 
 # %%
-df_capacity_s_10 = pd.read_csv("result/capacity{'user': 300, 'server': 10}.csv", names=(cp, ct))
-df_capacity_s_15 = pd.read_csv("result/capacity{'user': 300, 'server': 15}.csv", names=(cp, ct))
-df_capacity_s_20 = pd.read_csv("result/capacity{'user': 300, 'server': 20}.csv", names=(cp, ct))
+# df_capacity_s_10 = pd.read_csv("result/capacity{'user': 300, 'server': 10}.csv", names=(cp, ct))
+# df_capacity_s_15 = pd.read_csv("result/capacity{'user': 300, 'server': 15}.csv", names=(cp, ct))
+# df_capacity_s_20 = pd.read_csv("result/capacity{'user': 300, 'server': 20}.csv", names=(cp, ct))
 
 # %%
-df_user_s_10_cplex = pd.read_csv("result/user_server_10_capacity_50_cplex.csv", names=(un, ctc, ctm))
-df_user_s_10_glpk = pd.read_csv("result/user_server_10_capacity_50_glpk.csv", names=(un, ctg, ctm))
+# df_user_s_10_cplex = pd.read_csv("result/user_server_10_capacity_50_cplex.csv", names=(un, ctc, ctes))
+# df_user_s_10_glpk = pd.read_csv("result/user_server_10_capacity_50_glpk.csv", names=(un, ctg, ctes))
+
+# %%
+df_scip = pd.read_csv("result/scip.csv", names=(un, ctsc, ctes))
+df_cplex = pd.read_csv("result/cplex.csv", names=(un, ctc, ctes))
+df_glpk = pd.read_csv("result/glpk.csv", names=(un, ctg, ctes))
 
 # %%
 Graph.initialize_rcparams()
 
 # %%
-plt.plot(df_user_s_10[un], df_user_s_10[ct], label=(r'$|{V_{\rm S}}| = 10$'), color='k', marker='x', linestyle='-')
-plt.plot(df_user_s_15[un], df_user_s_15[ct], label=(r'$|{V_{\rm S}}| = 15$'), color='k', marker='^', linestyle='--')
-plt.plot(df_user_s_20[un], df_user_s_20[ct], label=(r'$|{V_{\rm S}}| = 20$'), color='k', marker='.', linestyle=':')
+# plt.plot(df_user_s_10[un], df_user_s_10[ct], label=(r'$|{V_{\rm S}}| = 10$'), color='k', marker='x', linestyle='-')
+# plt.plot(df_user_s_15[un], df_user_s_15[ct], label=(r'$|{V_{\rm S}}| = 15$'), color='k', marker='^', linestyle='--')
+# plt.plot(df_user_s_20[un], df_user_s_20[ct], label=(r'$|{V_{\rm S}}| = 20$'), color='k', marker='.', linestyle=':')
 
-plt.xticks([0, 100, 200, 300, 400])
-plt.yticks([0, 50, 100, 150, 200])
-plt.xlim((0, 400))
-plt.ylim((0, 200))
+# plt.xticks([0, 100, 200, 300, 400])
+# plt.yticks([0, 50, 100, 150, 200])
+# plt.xlim((0, 400))
+# plt.ylim((0, 200))
 
-plt.xlabel(un + ', ' + r'$|{V_{\rm U}}|$')
-plt.ylabel(ct + ' [s]')
+# plt.xlabel(un + ', ' + r'$|{V_{\rm U}}|$')
+# plt.ylabel(ct + ' [s]')
 
-plt.legend(loc="upper left")
+# plt.legend(loc="upper left")
 
-plt.savefig('graph/user.pdf')
-plt.show()
-plt.close()
-
-# %%
-plt.plot(df_capacity_s_10[cp], df_capacity_s_10[ct], label=(r'$|{V_{\rm S}}| = 10$'), color='k', marker='x', linestyle='-')
-plt.plot(df_capacity_s_15[cp], df_capacity_s_15[ct], label=(r'$|{V_{\rm S}}| = 15$'), color='k', marker='^', linestyle='--')
-plt.plot(df_capacity_s_20[cp], df_capacity_s_20[ct], label=(r'$|{V_{\rm S}}| = 20$'), color='k', marker='.', linestyle=':')
-
-plt.xticks([30, 40, 50, 60, 70])
-plt.yticks([0, 50, 100, 150, 200])
-plt.xlim((30, 70))
-plt.ylim((0, 200))
-
-plt.xlabel(cp + ', ' + r'$M_s$')
-plt.ylabel(ct + ' [s]')
-
-plt.legend(loc="upper left")
-
-plt.savefig('graph/capacity.pdf')
-plt.show()
-plt.close()
+# plt.savefig('graph/user.pdf')
+# plt.show()
+# plt.close()
 
 # %%
-plt.plot(df_user_s_10_cplex[un], df_user_s_10_cplex[ctm], label=('MMD'), color='k', marker='x', linestyle='-')
-plt.plot(df_user_s_10_cplex[un], df_user_s_10_cplex[ctc], label=('CPLEX'), color='k', marker='^', linestyle='--')
-plt.plot(df_user_s_10_glpk[un], df_user_s_10_glpk[ctg], label=('GLPK'), color='k', marker='.', linestyle=':')
+# plt.plot(df_capacity_s_10[cp], df_capacity_s_10[ct], label=(r'$|{V_{\rm S}}| = 10$'), color='k', marker='x', linestyle='-')
+# plt.plot(df_capacity_s_15[cp], df_capacity_s_15[ct], label=(r'$|{V_{\rm S}}| = 15$'), color='k', marker='^', linestyle='--')
+# plt.plot(df_capacity_s_20[cp], df_capacity_s_20[ct], label=(r'$|{V_{\rm S}}| = 20$'), color='k', marker='.', linestyle=':')
+
+# plt.xticks([30, 40, 50, 60, 70])
+# plt.yticks([0, 50, 100, 150, 200])
+# plt.xlim((30, 70))
+# plt.ylim((0, 200))
+
+# plt.xlabel(cp + ', ' + r'$M_s$')
+# plt.ylabel(ct + ' [s]')
+
+# plt.legend(loc="upper left")
+
+# plt.savefig('graph/capacity.pdf')
+# plt.show()
+# plt.close()
+
+# %%
+plt.plot(df_scip[un], df_scip[ctes], label=('ESUM'), color='k', marker='x', linestyle='-')
+plt.plot(df_glpk[un], df_glpk[ctg], label=('GLPK'), color='k', marker='s', linestyle='-.')
+plt.plot(df_scip[un], df_scip[ctsc], label=('SCIP'), color='k', marker='^', linestyle='--')
+plt.plot(df_cplex[un], df_cplex[ctc], label=('CPLEX'), color='k', marker='o', linestyle=':')
 
 plt.xlabel(un + ', ' + r'$|{V_{\rm U}}|$')
 plt.ylabel(ct + ' [s]')
