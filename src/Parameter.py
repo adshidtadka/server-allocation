f3434a52e7b61862dae652717ca71728d2f27a3a
adshidtadka <nextage900fx@gmail.com>
2019-12-17 13:53:29 +0900

    d_stを作成

--- src/Parameter.py
+++ src/Parameter.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import itertools
 import sys
+import math
 
 import Constant
 
@@ -23,12 +24,31 @@ class Parameter:
         self.DELAY_SERVER_MAX = Parameter.DELAY_SERVER_MAX_CONST
 
     def create_input(self):
-        # inputs
         self.e_u = list(itertools.product(range(self.USER_NUM), range(self.SERVER_NUM)))
         self.e_s = list(itertools.combinations(list(range(0, self.SERVER_NUM)), 2))
         self.d_us = np.random.randint(1, self.DELAY_USER_MAX, (self.USER_NUM, self.SERVER_NUM))
-        self.d_st = np.random.randint(1, self.DELAY_SERVER_MAX, len(list(itertools.combinations(list(range(0, self.SERVER_NUM)), 2))))
+        self.d_st = np.random.randint(1, self.DELAY_SERVER_MAX, len(self.e_s))
         self.m_s = np.full(self.SERVER_NUM, self.CAPACITY)
+
+    def kanto_input(self):
+        df_kanto = pd.read_csv("../network/kanto.csv")
+        self.SERVER_NUM_KANTO = len(df_kanto)
+        self.e_u = list(itertools.product(range(self.USER_NUM), range(self.SERVER_NUM_KANTO)))
+        self.e_s = list(itertools.combinations(list(range(0, self.SERVER_NUM_KANTO)), 2))
+        self.m_s = np.full(self.SERVER_NUM_KANTO, self.CAPACITY)
+
+        # 2点間の距離
+        def get_distance(x_1, y_1, x_2, y_2): return math.sqrt((x_2 - x_1) ** 2 + (y_2 - y_1) ** 2)
+
+        # d_stを作成
+        d_st = []
+        for link in self.e_s:
+            city_1, city_2 = link[0], link[1]
+            x_1, y_1 = df_kanto.iloc[city_1]["latitude"], df_kanto.iloc[city_1]["longitude"]
+            x_2, y_2 = df_kanto.iloc[city_2]["latitude"], df_kanto.iloc[city_2]["longitude"]
+            d_st.append(get_distance(x_1, y_1, x_2, y_2))
+        self.d_st = np.array(d_st)
+        print(self.d_st)
 
     def set_param(self, var_name, consts, var, delay_params):
         if var_name == 'user':
@@ -58,3 +78,12 @@ class Parameter:
             return Parameter.CAPACITY_CONST
         else:
             sys.exit('invalid var_name = ' + str(var_name))
+
+
+def main():
+    param = Parameter(1)
+    param.kanto_input()
+
+
+if __name__ == '__main__':
+    main()
