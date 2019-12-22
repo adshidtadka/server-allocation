b98aa3fe103938707489030f318e3a8ea11669f3
adshidtadka <nextage900fx@gmail.com>
2019-12-22 10:43:39 +0900

    命名方法を変更

--- src/cpp/hopcroft_karp.cpp
+++ src/cpp/hopcroft_karp.cpp
@@ -5,27 +5,28 @@
 #include <iostream>
 #include <list>
 #include <queue>
-#define NIL 0
-#define INF INT_MAX
+
+const int NIL = 0;
+const int INF = INT_MAX;
 
 // A class to represent Bipartite graph for
 // Hopcroft Karp implementation
-class BGraph {
-    // m and n are number of vertices on left
+class HopcroftKarp {
+    // leftNum and rightNum are number of vertices on left
     // and right sides of Bipartite Graph
-    int m, n;
+    int leftNum, rightNum;
 
-    // adj[u] stores adjacents of left side
-    // vertex 'u'. The value of u ranges from 1 to m.
+    // adjacent[u] stores adjacents of left side
+    // vertex 'u'. The value of u ranges from 1 to leftNum.
     // 0 is used for dummy vertex
-    std::list<int> *adj;
+    std::list<int> *adjacent;
 
     // pointers for hopcroftKarp()
-    int *pair_u, *pair_v, *dist;
+    int *uPair, *vPair, *distance;
 
 public:
-    BGraph(int m, int n);        // Constructor
-    void addEdge(int u, int v);  // To add edge
+    HopcroftKarp(int leftNum, int rightNum);  // Constructor
+    void addEdge(int u, int v);               // To add edge
 
     // Returns true if there is an augmenting path
     bool bfs();
@@ -35,25 +36,25 @@ public:
     bool dfs(int u);
 
     // Returns size of maximum matching
-    int hopcroftKarpAlgorithm();
+    int matching();
 };
 
 // Returns size of maximum matching
-int BGraph::hopcroftKarpAlgorithm() {
-    // pair_u[u] stores pair of u in matching on left side of Bipartite Graph.
-    // If u doesn't have any pair, then pair_u[u] is NIL
-    pair_u = new int[m + 1];
+int HopcroftKarp::matching() {
+    // uPair[u] stores pair of u in matching on left side of Bipartite Graph.
+    // If u doesn't have any pair, then uPair[u] is NIL
+    uPair = new int[leftNum + 1];
 
-    // pair_v[v] stores pair of v in matching on right side of Biparite Graph.
-    // If v doesn't have any pair, then pair_u[v] is NIL
-    pair_v = new int[n + 1];
+    // vPair[v] stores pair of v in matching on right side of Bipartite Graph.
+    // If v doesn't have any pair, then uPair[v] is NIL
+    vPair = new int[rightNum + 1];
 
-    // dist[u] stores distance of left side vertices
-    dist = new int[m + 1];
+    // distance[u] stores distance of left side vertices
+    distance = new int[leftNum + 1];
 
     // Initialize NIL as pair of all vertices
-    for (int u = 0; u <= m; u++) pair_u[u] = NIL;
-    for (int v = 0; v <= n; v++) pair_v[v] = NIL;
+    for (int u = 0; u <= leftNum; u++) uPair[u] = NIL;
+    for (int v = 0; v <= rightNum; v++) vPair[v] = NIL;
 
     // Initialize result
     int result = 0;
@@ -62,37 +63,37 @@ int BGraph::hopcroftKarpAlgorithm() {
     // augmenting path possible.
     while (bfs()) {
         // Find a free vertex to check for a matching
-        for (int u = 1; u <= m; u++)
+        for (int u = 1; u <= leftNum; u++)
 
             // If current vertex is free and there is
             // an augmenting path from current vertex
             // then increment the result
-            if (pair_u[u] == NIL && dfs(u)) result++;
+            if (uPair[u] == NIL && dfs(u)) result++;
     }
     return result;
 }
 
 // Returns true if there is an augmenting path available, else returns false
-bool BGraph::bfs() {
+bool HopcroftKarp::bfs() {
     std::queue<int> q;  // an integer queue for bfs
 
     // First layer of vertices (set distance as 0)
-    for (int u = 1; u <= m; u++) {
+    for (int u = 1; u <= leftNum; u++) {
         // If this is a free vertex, add it to queue
-        if (pair_u[u] == NIL) {
+        if (uPair[u] == NIL) {
             // u is not matched so distance is 0
-            dist[u] = 0;
+            distance[u] = 0;
             q.push(u);
         }
 
-        // Else set distance as infinite so that this vertex is considered next
-        // time for availibility
+        // Else set distance as infinite so that this vertex is considered
+        // next time for availability
         else
-            dist[u] = INF;
+            distance[u] = INF;
     }
 
     // Initialize distance to NIL as infinite
-    dist[NIL] = INF;
+    distance[NIL] = INF;
 
     // q is going to contain vertices of left side only.
     while (!q.empty()) {
@@ -101,78 +102,79 @@ bool BGraph::bfs() {
         q.pop();
 
         // If this node is not NIL and can provide a shorter path to NIL then
-        if (dist[u] < dist[NIL]) {
+        if (distance[u] < distance[NIL]) {
             // Get all the adjacent vertices of the dequeued vertex u
             std::list<int>::iterator it;
-            for (it = adj[u].begin(); it != adj[u].end(); ++it) {
+            for (it = adjacent[u].begin(); it != adjacent[u].end(); ++it) {
                 int v = *it;
 
                 // If pair of v is not considered so far
-                // i.e. (v, pair_v[v]) is not yet explored edge.
-                if (dist[pair_v[v]] == INF) {
+                // i.edgeNum. (v, vPair[v]) is not yet explored edge.
+                if (distance[vPair[v]] == INF) {
                     // Consider the pair and push it to queue
-                    dist[pair_v[v]] = dist[u] + 1;
-                    q.push(pair_v[v]);
+                    distance[vPair[v]] = distance[u] + 1;
+                    q.push(vPair[v]);
                 }
             }
         }
     }
 
-    // If we could come back to NIL using alternating path of distinct
+    // If we could come back to NIL using alternating path of distance
     // vertices then there is an augmenting path available
-    return (dist[NIL] != INF);
+    return (distance[NIL] != INF);
 }
 
 // Returns true if there is an augmenting path beginning with free vertex u
-bool BGraph::dfs(int u) {
+bool HopcroftKarp::dfs(int u) {
     if (u != NIL) {
         std::list<int>::iterator it;
-        for (it = adj[u].begin(); it != adj[u].end(); ++it) {
+        for (it = adjacent[u].begin(); it != adjacent[u].end(); ++it) {
             // Adjacent vertex of u
             int v = *it;
 
-            // Follow the distances set by BFS search
-            if (dist[pair_v[v]] == dist[u] + 1) {
-                // If dfs for pair of v also returnn true then
-                if (dfs(pair_v[v]) ==
-                    true) {  // new matching possible, store the matching
-                    pair_v[v] = u;
-                    pair_u[u] = v;
+            // Follow the distance set by BFS search
+            if (distance[vPair[v]] == distance[u] + 1) {
+                // If dfs for pair of v also return true then
+                if (dfs(vPair[v]) == true) {
+                    // new matching possible, store the matching
+                    vPair[v] = u;
+                    uPair[u] = v;
                     return true;
                 }
             }
         }
 
         // If there is no augmenting path beginning with u then.
-        dist[u] = INF;
+        distance[u] = INF;
         return false;
     }
     return true;
 }
 
 // Constructor for initialization
-BGraph::BGraph(int m, int n) {
-    this->m = m;
-    this->n = n;
-    adj = new std::list<int>[m + 1];
+HopcroftKarp::HopcroftKarp(int leftNum, int rightNum) {
+    this->leftNum = leftNum;
+    this->rightNum = rightNum;
+    adjacent = new std::list<int>[leftNum + 1];
 }
 
 // function to add edge from u to v
-void BGraph::addEdge(int u, int v) {
-    adj[u + 1].push_back(v + 1);  // Add v to u’s list.
+void HopcroftKarp::addEdge(int u, int v) {
+    adjacent[u + 1].push_back(v + 1);  // Add v to u’s list.
 }
 
 int main() {
-    int v1, v2, e;
-    std::cin >> v1 >> v2 >> e;  // vertices of left side, right side and edges
-    BGraph g(v1, v2);           //
+    int leftNum, rightNum, edgeNum;
+    std::cin >> leftNum >> rightNum >> edgeNum;
+    // vertices of left side, right side and edges
+    HopcroftKarp hc(leftNum, rightNum);
     int u, v;
-    for (int i = 0; i < e; ++i) {
+    for (int i = 0; i < edgeNum; ++i) {
         std::cin >> u >> v;
-        g.addEdge(u, v);
+        hc.addEdge(u, v);
     }
 
-    int res = g.hopcroftKarpAlgorithm();
+    int res = hc.matching();
     std::cout << "Maximum matching is " << res << "\n";
 
     return 0;
