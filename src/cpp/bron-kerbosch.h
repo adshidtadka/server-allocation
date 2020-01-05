0675dad861620b1609675a1de3a490ef549938fe
adshidtadka <nextage900fx@gmail.com>
2020-01-05 18:22:18 +0900

    bron-kerboschを引用する

---
+++ src/cpp/bron-kerbosch.h
@@ -0,0 +1,49 @@
+#ifndef BRON_KERBOSCH_H
+#define BRON_KERBOSCH_H
+
+#include <algorithm>
+#include <forward_list>
+#include <functional>
+#include <unordered_set>
+
+namespace BronKerbosch {
+
+template <typename T>
+struct Vertex {
+    Vertex(const T id) : id{id} {}
+
+    bool operator==(const Vertex& other) const { return this->id == other.id; }
+
+    T id;
+    std::unordered_set<T> ns;
+};
+
+template <typename T>
+using Graph = std::forward_list<Vertex<T> >;
+template <typename T>
+using Clique = Graph<T>;
+
+template <typename T>
+void solve(Graph<T> R, Graph<T> P, Graph<T> X,
+           std::function<void(Graph<T>, Graph<T>, Graph<T>)> act) {
+    if (P.empty() && X.empty()) {
+        act(R, P, X);
+        return;
+    }
+    while (!P.empty()) {
+        auto Ri = R;
+        auto Pi = P;
+        auto Xi = X;
+        const auto v = P.front();
+        Ri.push_front(v);
+        Pi.remove_if([&v](const Vertex<T>& p) { return !v.ns.count(p.id); });
+        Xi.remove_if([&v](const Vertex<T>& x) { return !v.ns.count(x.id); });
+        solve<T>(Ri, Pi, Xi, act);
+        P.remove(v);
+        X.push_front(v);
+    }
+}
+
+}  // namespace BronKerbosch
+
+#endif  // BRON_KERBOSCH_H
